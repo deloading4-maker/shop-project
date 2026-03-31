25cc6c91f8699e4973df788832910931a4b6befa
deloading4-maker <deloading4@gmail.com>
2026-03-31 18:04:04 +0300

    added backend

--- backend/server.js
+++ backend/server.js
@@ -19,27 +19,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
-
-const Product = mongoose.model("Product", {
-  name: String,
-  price: Number,
-  img: String
-});
-
-app.post("/login", (req, res) => {
-  const { login, password } = req.body;
-
-  if (login === "admin" && password === "123") {
-    res.json({ success: true });
-  } else {
-    res.status(401).json({ success: false });
-  }
-});
-
-const mongoose = require("mongoose");
-
-mongoose.connect("mongodb+srv://deloading4_db_user:howlor123@cluster0.yelzxdr.mongodb.net/?appName=Cluster0")
-  .then(() => console.log("MongoDB подключен"))
-  .catch(err => console.log(err));
-
-  console.log("test deploy");
